a8f0a2eb4f65e483c0beff06699b083b6f5cbcf5
Yan Golovchenko <y4nweb@gmail.com>
2024-04-13 21:49:51 +0300

    push

--- src/components/SearchPanel/SearchPanel.tsx
+++ src/components/SearchPanel/SearchPanel.tsx
@@ -1,5 +1,11 @@
+import style from "../SearchPanel/SearchPanel.module.scss";
+
 const SearchPanel = () => {
-  return <div>Search</div>;
+  return (
+    <div className={style.search}>
+      <input className={style.panel} type="text" placeholder="Search" />
+    </div>
+  );
 };
 
 export { SearchPanel };
